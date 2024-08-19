4adf718854d5f75a9b5ea5c1430422ea2b7073f1
HomunMage <homun@posetmage.com>
2024-08-19 22:13:50 +0800

    merge component

--- src/App.js
+++ src/App.js
@@ -9,22 +9,19 @@ import ChatLayout from './ChatBot/ChatLayout';
 import { ChapterProvider } from './JsonState';
 import Editor from './Editor/EditorLayout';
 import Test from './Test/Test';
-import { HistoryProvider } from './HistoryHandler'; // Import HistoryProvider
+import { HistoryProvider } from './HistoryHandler';
+import GraphApp from './Graph/GraphApp'; // Import the GraphApp
 
-// Component to conditionally apply StrictMode
 const ConditionalStrictMode = ({ children }) => {
   const location = useLocation();
-  
-  // Apply StrictMode to all routes except '/editor'
   const useStrictMode = location.pathname !== '/editor';
-
   return useStrictMode ? <React.StrictMode>{children}</React.StrictMode> : <>{children}</>;
 };
 
 function App() {
   return (
     <ChapterProvider>
-      <HistoryProvider> {/* Wrap with HistoryProvider */}
+      <HistoryProvider>
         <Router>
           <div className="App">
             <Body />
@@ -33,6 +30,7 @@ function App() {
                 <Route path="/" element={<ChatLayout />} />
                 <Route path="/editor" element={<Editor />} />
                 <Route path="/test" element={<Test />} />
+                <Route path="/graph" element={<GraphApp />} /> {/* Route for GraphApp */}
               </Routes>
             </ConditionalStrictMode>
             <Footer />
