64bf848157e97902a7588b3eb0be1fa7af516533
HomunMage <homun@posetmage.com>
2024-08-20 18:53:17 +0800

    only gen 1 node, todo: debug

--- src/ConvertGraph.js
+++ src/ConvertGraph.js
@@ -1,14 +1,42 @@
 // ConvertGraph.js
 import { useHistory } from './HistoryHandler';
 import { useCallback } from 'react';
+import { useGraphManager } from './Graph/GraphManagerContext';
 
 // Function to process promptMap data
-const processPrompts = (promptMap) => {
+const processPrompts = (promptMap, nodes, setNodes, nodeIdCounter, setNodeIdCounter) => {
   if (promptMap && promptMap.size > 0) {
+    let xOffset = 0;
+    const newNodes = [];
+
     promptMap.forEach((prompt, key) => {
       console.log(`Key: ${key}`);
       console.log(`Prompt: ${prompt}`);
+
+      const newNode = {
+        id: nodeIdCounter.toString(),
+        type: 'textUpdater',
+        data: { 
+          name: key, 
+          description: prompt, 
+          type: 'STEP', 
+          nexts: [], 
+          true_next: null, 
+          false_next: null, 
+          width: 200, 
+          height: 200 
+        },
+        position: { x: xOffset, y: 0 },
+        prevs: []
+      };
+
+      newNodes.push(newNode);
+      setNodeIdCounter(prevCounter => prevCounter + 1);
+
+      xOffset += 50; // Increase x-offset by 250 to prevent overlap
     });
+
+    setNodes(prevNodes => [...prevNodes, ...newNodes]);
   } else {
     console.log('No prompts available.');
   }
@@ -17,7 +45,9 @@ const processPrompts = (promptMap) => {
 // Custom hook to provide the processPrompts function
 export const useConvertGraph = () => {
   const { promptMap } = useHistory();
+  const { nodes, setNodes, nodeIdCounter, setNodeIdCounter } = useGraphManager();
+
   return useCallback(() => {
-    processPrompts(promptMap);
-  }, [promptMap]);
+    processPrompts(promptMap, nodes, setNodes, nodeIdCounter, setNodeIdCounter);
+  }, [promptMap, nodes, setNodes, nodeIdCounter, setNodeIdCounter]);
 };
